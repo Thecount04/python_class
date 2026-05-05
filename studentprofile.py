9c652f1a9299c07ab876d21835a31561231fa5a3
Thecount04 <naseribeseka01@gmail.com>
2026-05-05 12:03:04 -0400

    first push

---
+++ studentprofile.py
@@ -0,0 +1,8 @@
+name = "Smith"
+age = 60
+course = "Python"
+city = "Virginia_Beach"
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("City:", city)
---
+++ variables.py
@@ -0,0 +1,8 @@
+name = "nas"
+age = 43
+favorite_color = "white"
+
+print(name)
+print(age)
+print(favorite_color)
+
